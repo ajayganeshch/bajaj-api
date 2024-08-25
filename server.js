d878ba49d503fe22c1784c3574caab1c8cb9cf78
Ajay Ganesh <87056991+ajayganeshch@users.noreply.github.com>
2024-08-25 15:50:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ const app = require("./app");
 //--------------- Server ---------------
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, "127.0.0.1", () => {
+const server = app.listen(port, "0.0.0.0", () => {
   console.log(port);
 });
